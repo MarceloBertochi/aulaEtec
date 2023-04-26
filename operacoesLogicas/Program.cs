94b64077c4e62891072c0eb0fcce728abae30877
MarceloBertochi <paidasamia@gmail.com>
2023-04-26 14:44:36 -0300

    Aula Etec operacoes lógicas

---
+++ operacoesLogicas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AulaOperacoesLogicas
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            // /vaciáveis Booleanas
+
+            int idade = 18;
+            string cnh = "S";           
+            
+
+            Console.Write("Digite sua idade..:");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Possui CNH (S/N)..:");
+            cnh = (Console.ReadLine()!.ToUpper());
+
+
+            if (idade >= 18 && cnh == "S")
+            {
+                Console.WriteLine("CNH Válida");
+            }
+
+            else
+            { Console.WriteLine("Você não tem idade para obter uma CNH"); }
+
+
+
+            //Utilizando Comparações
+            //Operador Ternário
+
+            /*
+                        //bool p = false, q = false, r = false; // 1
+                        //bool p = false, q = true, r = false; // 2
+                        //bool p = true, q = false, r = false; // 3
+                        //bool p = true, q = true, r = false; // 4
+                        //bool p = false, q = false, r = true; // 5
+                        //bool p = false, q = true, r = true; // 6
+                        //bool p = true, q = false, r = true; // 7
+                        //bool p = true, q = true, r = true; // 8
+
+
+
+
+                        Console.WriteLine(!p || q && r);
+                        //Console.WriteLine((p || q) && (!r));
+                        */
+        }
+    }
+}
