74a145c9b1dbb62c9a24992580e3ba9cb2811a11
MarceloBertochi <paidasamia@gmail.com>
2023-05-13 22:51:32 -0300

    Aula de rotinas com strings

--- rotinas/Program.cs
+++ rotinas/Program.cs
@@ -10,23 +10,28 @@ namespace Rotinas
 
         {
             ExibirHoraAtual();
-            AguardarUsuario("Digite uma tecla");
+            AguardarUsuario();
             ExibirHoraAtual();
             AguardarUsuario("Digite uma outra tecla");
             ExibirHoraAtual();
         }
 
-        void ExibirHoraAtual() // rotina para exibir a hora atual 
+        static void ExibirHoraAtual() // rotina para exibir a hora atual 
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine(DateTime.Now.ToLongDateString());
             Console.ResetColor();
         }
 
-        void AguardarUsuario(string mensagem)
+        static void AguardarUsuario(string mensagem)
         {
             Console.WriteLine(mensagem);
             Console.ReadKey(); // somente espera qualquer tecla ser acionada.
+        }
+
+        static void AguardarUsuario()
+        {
+            AguardarUsuario("Digite uma tecla");
         }
     }
 }
