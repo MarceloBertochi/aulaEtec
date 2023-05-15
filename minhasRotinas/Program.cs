7f3f18b0a3768c86f8c2dd4e216b6136c6132023
MarceloBertochi <paidasamia@gmail.com>
2023-05-15 15:44:24 -0300

    Rotina da minha assinatura

---
+++ minhasRotinas/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ElaborandoMinhasRotinas
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            LimparTela();
+            MinhaAssinatura();
+            ResetarCor();
+        }
+
+        static void MinhaAssinatura()
+        {
+            LinhaAmarela();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Trabalho para Nota do Aluno ");
+            BarraAmarelaFinal();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Marcelo Camara Bertochi ");
+            BarraAmarelaFinal2();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Curso Informática I - Noite ");
+            BarraAmarelaFinal();
+            LinhaAmarela();
+        }
+
+        static void LinhaAmarela()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("-------------------------------\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaInicio()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|");
+            Console.ResetColor();
+        }
+        static void BarraAmarelaFinal()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaFinal2()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n".PadLeft(6, ' '));
+            Console.ResetColor();
+        }
+        static void ResetarCor()
+        {
+            Console.ResetColor();
+        }
+
+        static void LimparTela()
+        {
+            Console.Clear();
+        }
+
+    }
+}
