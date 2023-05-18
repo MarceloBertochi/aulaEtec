f1805a8409bab5d7787a73558646db47047a015f
MarceloBertochi <paidasamia@gmail.com>
2023-05-18 15:28:53 -0300

    rotina com string

---
+++ AprendendoRotina/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RotinaString
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            string nome, sobrenome;
+
+            Console.Clear();
+            Console.Write("digite seu nome..: ");
+            nome = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Console.Write("digite seu sobrenome..: ");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine();
+
+            string resultado = ConcatenarTexto(nome, sobrenome);
+
+            Console.WriteLine(resultado);
+
+        }
+        static string ConcatenarTexto(string texto1, string texto2)
+        {
+            string SomaDosTextos = texto1 + " " + texto2;
+            return SomaDosTextos;
+        }
+    }
+}
+
+/*
+static string MeuNome()
+        {
+            string MeuNome = "Marcelo Bertochi";
+            return MeuNome;
+        }
+*/
+
+
