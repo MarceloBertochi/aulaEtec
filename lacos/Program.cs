bf96e50640a9ef65a4762b58e9febe87dd9100fa
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-19 20:30:01 -0300

    Aula de laços parte 2

--- lacos/Program.cs
+++ lacos/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Laços
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+           string opcao = "";
+            while (opcao != "X")
+            {
+                Console.WriteLine("OI");
+                Console.Write("dESEJA CONSINUAR");
+                opcao = Console.ReadLine()!;
+
+                Console.WriteLine($"voce digitou [{opcao}]");
+            }
+
+
+        }
+    }
+}
+
+/*
+ Console.Clear();
+            int contador = 1;
+            const int max = 10;
+
+             Console.WriteLine("Inicio da execução");
+
+            while (contador <= 10)
+
+            {
+                Console.WriteLine($"Execução # {contador}");
+                Thread.Sleep(1500);
+                contador = contador + 1;
+            }
+
+            Console.WriteLine("Fim da execução");
+*/
